736d2fb2729002396f352263c6105e088b40280a
johnarathy <john.arathy@gmail.com>
2020-06-05 01:23:07 +0530

    added InsertionSort file

--- src/com/qa/sortingalgos/InsertionSort.java
+++ src/com/qa/sortingalgos/InsertionSort.java
@@ -1,10 +1,10 @@
 package com.qa.sortingalgos;
 
-public class SelectionSort {
+public class InsertionSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("I just want to edit for git commit");
+System.out.println("Sample code for insertion sort");
 	}
 
 }
